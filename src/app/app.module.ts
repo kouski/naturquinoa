c68f72f9b6a924bfac4d117215ce81719562f18b
kouski <72550619+kouski@users.noreply.github.com>
2024-01-03 10:28:20 +0100

    Copia archivos proyecto bootcamp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatChipsModule} from '@angular/material/chips';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [	
     AppComponent,
+    FooterComponent,
+    NavbarComponent
       
       
    ],
